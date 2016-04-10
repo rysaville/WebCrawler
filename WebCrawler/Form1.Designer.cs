03c52275e23b015d13e0a6317546eb60c361c32e
rysaville <ryansaville2008@gmail.com>
2016-04-09 20:50:33 -0700

    stuff

--- WebCrawler/Form1.Designer.cs
+++ WebCrawler/Form1.Designer.cs
@@ -28,12 +28,94 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbInput = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.tbOutput = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(242, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(275, 55);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Web Crawler";
+			// 
+			// tbInput
+			// 
+			this.tbInput.Location = new System.Drawing.Point(45, 119);
+			this.tbInput.Multiline = true;
+			this.tbInput.Name = "tbInput";
+			this.tbInput.Size = new System.Drawing.Size(575, 93);
+			this.tbInput.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(55, 103);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(31, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Input";
+			// 
+			// tbOutput
+			// 
+			this.tbOutput.Location = new System.Drawing.Point(45, 329);
+			this.tbOutput.Multiline = true;
+			this.tbOutput.Name = "tbOutput";
+			this.tbOutput.Size = new System.Drawing.Size(575, 92);
+			this.tbOutput.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(58, 310);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(39, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Output";
+			// 
+			// btnGo
+			// 
+			this.btnGo.Location = new System.Drawing.Point(61, 258);
+			this.btnGo.Name = "btnGo";
+			this.btnGo.Size = new System.Drawing.Size(75, 23);
+			this.btnGo.TabIndex = 5;
+			this.btnGo.Text = "Go";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(714, 488);
+			this.Controls.Add(this.btnGo);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.tbOutput);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.tbInput);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbInput;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox tbOutput;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnGo;
 	}
 }
 
--- WebCrawler/Form1.cs
+++ WebCrawler/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,37 @@ namespace WebCrawler
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnGo_Click(object sender, EventArgs e)
+		{
+			tbOutput.Text = string.Empty;
+			string[] strURLs = tbInput.Text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			foreach (string url in strURLs)
+			{
+				if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+				{
+					HttpWebRequest wrGet = WebRequest.Create(url) as HttpWebRequest;
+					try
+					{
+						HttpWebResponse wrRespond = wrGet.GetResponse() as HttpWebResponse;
+						tbOutput.Text += wrRespond.StatusCode.ToString() + " : " + url + Environment.NewLine;
+					}
+					catch (WebException ex)
+					{
+						HttpWebResponse errorResponse = ex.Response as HttpWebResponse;
+						tbOutput.Text += errorResponse.StatusCode.ToString() + " : " + url;
+					}
+					catch (Exception exBad)
+					{
+						tbOutput.Text = "A Bad error occured:" + exBad.ToString();
+					}
+				}
+				else
+				{
+					tbOutput.Text += "Not in a valid format:" + url + Environment.NewLine;
+				}
+			}
 		}
 	}
 }
